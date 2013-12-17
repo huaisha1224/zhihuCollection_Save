996a4f506a80800678f592538db87528dfa8f0e3
Sam.Huang <sam.hxq@gmail.com>
2013-12-17 09:47:56 +0800

    2013.12.17更新 1、修正由于知乎网页改版之后原脚本失效的问题 2、修正老版本中不能转发超过20条收藏文章的BUG

--- zhihu_to_evernote.py
+++ zhihu_to_evernote.py
@@ -4,11 +4,14 @@
 """
 Copyright 2013 zhihu_to_evernote
 =======================================
-author          = "Sam Huang"
-name            = "zhihu_to_evernote"
-version         = "1.0.1"
-url             = "http://www.hiadmin.org"
-author_email    = "sam.hxq@gmail.com"
+author              = "Sam Huang"
+name                = "zhihu_to_evernote"
+version             = "1.0.5"
+url                 = "http://www.hiadmin.org"
+author_email        = "sam.hxq@gmail.com"
+Python_ver          = "Python3.x"
+Requests_ver        = "Requests2.1.0"
+BeautifulSoup_ver   = "BS4.2"
 =========================================
 
 将知乎收藏问答页面的所有问答发送到Evernote/印象笔记中、
@@ -28,41 +31,80 @@ notebook = 知乎收藏文章
 """
 import os
 import re
+import sys
 import requests
 import configparser
 import smtplib
+from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
-#from email.mime.image import MIMEImag
 
 
 
-def collect_url(url):
+#=============================================================================
+def Next_page(url):
+    """
+    接收用户输入的知乎收藏页面地址、提取所有分页的url地址存入列表中
+    通过正则表达式匹配内容并提取出来所有分页地址的url
+    @url_list_regex :提取URL规则、如果为空说明本页已没收藏的文章
+    @page_url       :用于存放所有分页url
+    """
+    url_list_regex = r"(<h2 class.*href=\")(/\w+/\d+)(\">)(.*)(</a></h2>)"
+    global page_url
+    page_url = []
+
+    #通过知乎页面规则来增加后面的分页数量、
+    #http://www.zhihu.com/collection/20261977?page=2 就表示第二页、以此类推。
+    for i in range(1,1000):
+        for x in url:
+            url_temp = url + str(i)
+        r = requests.get(url_temp)
+        if r.status_code == 200:
+            text_temp = r.text
+        #用正则表达式去匹配是否有收藏内容，如果有则添加到列表中，否则循环中断
+            if re.search(url_list_regex, text_temp) != None: 
+                page_url.append(url_temp)
+            else:
+                print ("No Next Page")
+                break
+
+
+
+
+#=============================================================================
+def Collect_url(url):
     """
     接收用户输入的知乎收藏页面地址、
     然后提取知乎收藏页面的URL地址
-    @url_list_regex       :提取URL规则、我们真正需要的是第二个()里面的内容
-    @url_list             :用于存放从收藏页面提取出来的URL地址列表
+    @url_list_regex         :提取URL规则、我们真正需要的是第二个()里面的内容
+    @single_url_list        :用于存放从单个收藏页面提取出来的URL地址列表
 
     """      
+
     url_list_regex = r"(<h2 class.*href=\")(/\w+/\d+)(\">)(.*)(</a></h2>)"
-    global url_list
-    url_list = []
+    global single_url_list
+    single_url_list = []
     list_temp = []
     r = requests.get(url)
     if r.status_code == 200:
         r_txt = r.text
+        #如果正则表达式匹配成功则说明里面有单个收藏链接、添加到列表中
         if re.search(url_list_regex,r_txt).group() != None:
             list_temp = re.findall(url_list_regex,r_txt)
-
+        else:
+            pass
+    
+    #用for循环吧列表里面的内容通过切片的时候提取出我们需要的，
+    #并赋值给single_url_list
     for i in list_temp:
-        url_list.append("http://www.zhihu.com"+i[1])
-    print (len(url_list))
+        single_url_list.append("http://www.zhihu.com"+i[1])
 
+    for x in single_url_list:#将单页面url提取出来增加到url总表中
+        url_list.append(x)
 
-
-def email_zhihu_content(url):
+#=============================================================================
+def Email_zhihu_content(url):
     """
     接收输入的单个收藏页面的URL地址、然后提取title、并将其作为邮件主题。
     用requests.get()下载页面内容、然后用smtplib发送html内容到
@@ -72,17 +114,19 @@ def email_zhihu_content(url):
     @
 
     """
-    title_name_regex = r"(<title>)(.*)(\s-)(.*</title>)"
+    #提取问题的标题，并将其作为邮件的主题
     r = requests.get(url)
     if r.status_code == 200:
         html_txt = r.text
-        title_name = re.search(title_name_regex,html_txt).group(2)
+        soup = BeautifulSoup(html_txt)
+        title_name = soup.title.string
         print (title_name)
-        subject = title_name
 
+
+        subject = title_name
         #设置邮件主题
         msg = MIMEMultipart("alternative")
-        msg["Subject"] = Header(subject + notebook,"utf-8")
+        msg["Subject"] = Header(title_name + notebook,"utf-8")
         part = MIMEText(html_txt,"html") #设置以html格式发送内容
         msg.attach(part)
         
@@ -91,21 +135,29 @@ def email_zhihu_content(url):
         smtp.connect(mail_host) 
         smtp.login(mail_user,mail_password) 
         smtp.sendmail(mail_user,evernote_mail,msg.as_string()) 
-        smtp.quit() 
-        
+        smtp.quit()
 
 
-
+#=============================================================================  
 if __name__ == "__main__":
+    global url_list
+    url_list = []
     #从配置文件读取内容
     cf = configparser.ConfigParser()
     cf.read("config.ini")
-    url = cf.get("info","url")
+    url = cf.get("info","url") + "?page="
     mail_host = cf.get("info","mail_host")
     mail_user = cf.get("info","mail_user")
     mail_password = cf.get("info","mail_password")
     evernote_mail = cf.get("info","evernote_mail")
     notebook = "@" + cf.get("info","notebook")
-    collect_url(url)
-    for i in url_list:
-        email_zhihu_content(i)
+
+    Next_page(url)
+
+    for i in page_url: #循环所有收藏文章分页列表
+        print (i)
+        Collect_url(i)
+    print (len(url_list))
+    for x in url_list: #循环所有的单页收藏文章url列表
+        print (x)
+        Email_zhihu_content(x)
